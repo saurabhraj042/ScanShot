867b425c0b9b78100f5ed1a0a3c54a985243910e
Saurabh Raj <saurabhraj042@gmail.com>
2020-07-22 15:13:04 +0530

    Added Scanbot SDK support with changes in UI

--- lib/ui/views/homepage.dart
+++ lib/ui/views/homepage.dart
@@ -1,43 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:scan_shot/core/viewmodels/auth_service.dart';
-import 'package:scan_shot/ui/views/login/login.dart';
+import 'package:scan_shot/ui/widgets/main_page_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomePageState createState() {
+    return _HomePageState();
+  }
 }
 
 class _HomePageState extends State<HomePage> {
-  AuthService _auth = AuthService();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ScanShot'),
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(
-            FontAwesomeIcons.signOutAlt,
-            size: 20.0,
-            color: Colors.white,
-          ),
-          onPressed: () {
-            _auth.signOutGoogle();
-            print('Signed out');
-            setState(() {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) {
-                    return Login();
-                  },
-                ),
-              );
-            });
-          },
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPageWidget(),
     );
   }
 }
---
+++ lib/ui/widgets/main_page_widget.dart
@@ -0,0 +1,305 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:scan_shot/core/viewmodels/auth_service.dart';
+import 'package:scan_shot/ui/scan_ui/menu_items.dart';
+import 'package:scan_shot/ui/scan_ui/preview_document_widget.dart';
+import 'package:scan_shot/ui/scan_ui/progress_dialog.dart';
+import 'package:scan_shot/ui/scan_ui/utils.dart';
+import 'package:scan_shot/ui/views/login/login.dart';
+import 'package:scanbot_sdk/barcode_scanning_data.dart';
+import 'package:scanbot_sdk/common_data.dart';
+import 'package:scanbot_sdk/document_scan_data.dart';
+import 'package:scanbot_sdk/ehic_scanning_data.dart';
+import 'package:scanbot_sdk/mrz_scanning_data.dart';
+import 'package:scanbot_sdk/scanbot_sdk.dart';
+import 'package:scanbot_sdk/scanbot_sdk_ui.dart';
+
+import '../../pages_repository.dart';
+
+class MainPageWidget extends StatefulWidget {
+  @override
+  _MainPageWidgetState createState() => _MainPageWidgetState();
+}
+
+class _MainPageWidgetState extends State<MainPageWidget> {
+  PageRepository _pageRepository = PageRepository();
+  AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              FontAwesomeIcons.signOutAlt,
+              size: 20.0,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              _auth.signOutGoogle();
+              print('Signed out');
+              setState(
+                () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Login();
+                      },
+                    ),
+                  );
+                },
+              );
+            },
+          )
+        ],
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text('ScanShot',
+            style: TextStyle(inherit: true, color: Colors.black)),
+      ),
+      body: ListView(
+        children: <Widget>[
+          TitleItemWidget("Document Scanner"),
+          MenuItemWidget(
+            "Scan Document",
+            onTap: () {
+              startDocumentScanning();
+            },
+          ),
+          MenuItemWidget(
+            "Import Image",
+            onTap: () {
+              importImage();
+            },
+          ),
+          MenuItemWidget(
+            "View Image Results",
+            endIcon: Icons.keyboard_arrow_right,
+            onTap: () {
+              gotoImagesView();
+            },
+          ),
+          TitleItemWidget("Data Detectors"),
+          MenuItemWidget(
+            "Scan Barcode (all formats: 1D + 2D)",
+            onTap: () {
+              startBarcodeScanner();
+            },
+          ),
+          MenuItemWidget(
+            "Scan QR code (QR format only)",
+            onTap: () {
+              startQRScanner();
+            },
+          ),
+          MenuItemWidget(
+            "Scan MRZ (Machine Readable Zone)",
+            onTap: () {
+              startMRZScanner();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  getOcrConfigs() async {
+    try {
+      var result = await ScanbotSdk.getOcrConfigs();
+      showAlertDialog(context, jsonEncode(result), title: "OCR Configs");
+    } catch (e) {
+      print(e);
+      showAlertDialog(context, "Error getting license status");
+    }
+  }
+
+  getLicenseStatus() async {
+    try {
+      var result = await ScanbotSdk.getLicenseStatus();
+      showAlertDialog(context, jsonEncode(result), title: "License Status");
+    } catch (e) {
+      print(e);
+      showAlertDialog(context, "Error getting OCR configs");
+    }
+  }
+
+  importImage() async {
+    try {
+      var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+      await createPage(image.uri);
+      gotoImagesView();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  createPage(Uri uri) async {
+    if (!await checkLicenseStatus(context)) {
+      return;
+    }
+
+    var dialog = ProgressDialog(context,
+        type: ProgressDialogType.Normal, isDismissible: false);
+    dialog.style(message: "Processing");
+    dialog.show();
+    try {
+      var page = await ScanbotSdk.createPage(uri, false);
+      page = await ScanbotSdk.detectDocument(page);
+      this._pageRepository.addPage(page);
+    } catch (e) {
+      print(e);
+    } finally {
+      dialog.hide();
+    }
+  }
+
+  startDocumentScanning() async {
+    if (!await checkLicenseStatus(context)) {
+      return;
+    }
+
+    DocumentScanningResult result;
+    try {
+      var config = DocumentScannerConfiguration(
+        bottomBarBackgroundColor: Colors.blue,
+        ignoreBadAspectRatio: true,
+        multiPageEnabled: true,
+        //maxNumberOfPages: 3,
+        //flashEnabled: true,
+        //autoSnappingSensitivity: 0.7,
+        cameraPreviewMode: CameraPreviewMode.FIT_IN,
+        orientationLockMode: CameraOrientationMode.PORTRAIT,
+        //documentImageSizeLimit: Size(2000, 3000),
+        cancelButtonTitle: "Cancel",
+        pageCounterButtonTitle: "%d Page(s)",
+        textHintOK: "Perfect, don't move...",
+        //textHintNothingDetected: "Nothing",
+        // ...
+      );
+      result = await ScanbotSdkUi.startDocumentScanner(config);
+    } catch (e) {
+      print(e);
+    }
+
+    if (isOperationSuccessful(result)) {
+      _pageRepository.addPages(result.pages);
+      gotoImagesView();
+    }
+  }
+
+  startBarcodeScanner() async {
+    if (!await checkLicenseStatus(context)) {
+      return;
+    }
+
+    try {
+      var config = BarcodeScannerConfiguration(
+        topBarBackgroundColor: Colors.blue,
+        finderTextHint:
+            "Please align any supported barcode in the frame to scan it.",
+        // ...
+      );
+      var result = await ScanbotSdkUi.startBarcodeScanner(config);
+      _showBarcodeScanningResult(result);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  startQRScanner() async {
+    if (!await checkLicenseStatus(context)) {
+      return;
+    }
+
+    try {
+      var config = BarcodeScannerConfiguration(
+        barcodeFormats: [BarcodeFormat.QR_CODE],
+        finderTextHint: "Please align a QR code in the frame to scan it.",
+        // ...
+      );
+      var result = await ScanbotSdkUi.startBarcodeScanner(config);
+      _showBarcodeScanningResult(result);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _showBarcodeScanningResult(final BarcodeScanningResult result) {
+    if (isOperationSuccessful(result)) {
+      showAlertDialog(
+          context,
+          "Format: " +
+              result.barcodeFormat.toString() +
+              "\nValue: " +
+              result.text,
+          title: "Barcode Result:");
+    }
+  }
+
+  startEhicScanner() async {
+    if (!await checkLicenseStatus(context)) {
+      return;
+    }
+
+    HealthInsuranceCardRecognitionResult result;
+    try {
+      var config = HealthInsuranceScannerConfiguration(
+        topBarBackgroundColor: Colors.blue,
+        topBarButtonsColor: Colors.white70,
+        // ...
+      );
+      result = await ScanbotSdkUi.startEhicScanner(config);
+    } catch (e) {
+      print(e);
+    }
+
+    if (isOperationSuccessful(result) && result?.fields != null) {
+      var concatenate = StringBuffer();
+      result.fields
+          .map((field) =>
+              "${field.type.toString().replaceAll("HealthInsuranceCardFieldType.", "")}:${field.value}\n")
+          .forEach((s) {
+        concatenate.write(s);
+      });
+      showAlertDialog(context, concatenate.toString());
+    }
+  }
+
+  startMRZScanner() async {
+    if (!await checkLicenseStatus(context)) {
+      return;
+    }
+
+    MrzScanningResult result;
+    try {
+      var config = MrzScannerConfiguration(
+        topBarBackgroundColor: Colors.blue,
+        // ...
+      );
+      result = await ScanbotSdkUi.startMrzScanner(config);
+    } catch (e) {
+      print(e);
+    }
+
+    if (isOperationSuccessful(result)) {
+      var concatenate = StringBuffer();
+      result.fields
+          .map((field) =>
+              "${field.name.toString().replaceAll("MRZFieldName.", "")}:${field.value}\n")
+          .forEach((s) {
+        concatenate.write(s);
+      });
+      showAlertDialog(context, concatenate.toString());
+    }
+  }
+
+  gotoImagesView() async {
+    imageCache.clear();
+    return await Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => DocumentPreview(_pageRepository)),
+    );
+  }
+}
