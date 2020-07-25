7aa28dbfc5b6ec4a4517a2d7faacbfced88c3708
Saurabh Raj <saurabhraj042@gmail.com>
2020-07-25 16:48:44 +0530

    final build

--- lib/ui/widgets/card_item.dart
+++ lib/ui/widgets/card_item.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Cards extends StatelessWidget {
+  final Color color ;
   final String title;
   final GestureTapCallback onTap;
   final IconData startIcon;
   final IconData endIcon;
 
-  const Cards(this.title, {this.onTap, this.startIcon, this.endIcon});
+  const Cards(this.title,this.color, {this.onTap, this.startIcon, this.endIcon});
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -16,13 +17,24 @@ class Cards extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: SizedBox(
           width: (width * 0.40),
-          height: (height* 0.20),
+          height: (height * 0.20),
           child: RaisedButton(
-            color: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                //side: BorderSide(color: Colors.black),
+                ),
+            color: color,
             elevation: 20,
             padding: const EdgeInsets.all(10),
             onPressed: onTap,
-            child: Text(title),
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black54,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           )),
     );
   }
